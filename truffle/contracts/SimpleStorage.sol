cb7d7521d7147c49a2ff5533ce77e05e10379528
chandresh2727 <trivedichandresh882@gmail.com>
2023-04-05 12:08:20 +0530

    Random Toss And choosing option updated

--- truffle/contracts/SimpleStorage.sol
+++ truffle/contracts/SimpleStorage.sol
@@ -2,13 +2,25 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract SimpleStorage {
-  uint256 value;
 
-  function read() public view returns (uint256) {
-    return value;
+  enum ChooseField {Tie,BAT,BALL}
+  ChooseField chooseField;
+
+  //This is for generating the random number
+  uint public randNumber = uint(keccak256(abi.encodePacked(block.timestamp,msg.sender))) % 2;
+  bool update;
+  // Function to check user won or not
+  function tossChoss(uint256 number) public returns(bool) {
+      if(number == randNumber){
+          update = true;
+          return true;
+      }
+      update = false;
+      return false;
   }
 
-  function write(uint256 newValue) public {
-    value = newValue;
-  }
+  function fieldChoss(uint _value) public {
+            require(uint(ChooseField.BALL) >= _value);
+            chooseField = ChooseField(_value);
+    }
 }
